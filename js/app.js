45994cc548e412bfdb91adc9787366e342ea0a7a
chocopros <jesuschock93@gmail.com>
2022-07-09 21:45:43 -0400

    Portafolio-V2-Start-Ok

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log("Jesus Arechider")
+
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".nav-menu")
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible")
+})
