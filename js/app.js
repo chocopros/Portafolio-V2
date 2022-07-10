a9a4c7795b55da8db14685455953d89b126274f5
chocopros <jesuschock93@gmail.com>
2022-07-10 17:28:15 -0400

    Portafolio-V2-Final_0.2

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-console.log("Jesus Arechider")
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
 
-const navToggle = document.querySelector(".nav-toggle")
-const navMenu = document.querySelector(".nav-menu")
+if(navMenu){
+    console.log(navMenu)
+}
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu_visible")
-})
+  navMenu.classList.toggle("nav-menu_visible");
+
+  if (navMenu.classList.contains("nav-menu_visible")) {
+    navToggle.setAttribute("aria-label", "Abrir menú");
+    console.log("close")
+  } else {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
+    console.log("open")
+  }
+});
